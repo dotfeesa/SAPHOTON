580b897d86d5ee7e64515523c225ca09342c17c6
Department of Telecommunications University of Sarajevo <52405258+dotfeesa@users.noreply.github.com>
2022-12-02 15:53:19 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# SAPHOTON
-Quantum Internet Hackathon 2022 - University of Sarajevo (Group 1)
+**Quantun Internet Hackathon 2022**
+
+University of Sarajevo
+Faculty of Electrical Engineering
+Department of Telecommunications
+
+**TEAM: PHOTONSA**
+
+**Challange 1** 
+https://github.com/hjir/QIH22-QKD/blob/main/CHALLENGE.md
+
+**Instructions for usage**
+https://github.com/hjir/QIH22-QKD
---
+++ qkd/input/eve.py
@@ -0,0 +1,30 @@
+import json
+import math
+import random
+
+from netqasm.sdk.qubit import Qubit
+
+class Eve:
+
+    def __init__(self):
+        pass
+
+    def eavesdrop(self, qubit: Qubit):
+        # IMPLEMENT YOUR EAVESDROPPING CODE HERE
+        #
+        # This method is called for each qubit individually at both ends of the
+        # connection. However, note that these will be actually two separate
+        # instances of Eve - one at each end. Thus, whilst you can share state
+        # from qubit to qubit, this state cannot be shared between the two ends
+        # of the connection.
+        #
+        # When measuring make sure to use `inplace=True` when calling
+        # `qubit.measure(inpace=True)` so that the qubit is still available when
+        # delivered to the application. Otherwise, the qubit is released.
+        basis_flips = random.randint(0, 1)
+        if basis_flips:
+            qubit.H()
+        measure = random.randint(0, 1)
+        if measure:
+            qubit.measure(inplace=True)
+        pass
